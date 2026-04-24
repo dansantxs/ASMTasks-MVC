5aad3b84ad4eb689f0a38a3e17f6eae0efe2a599
Daniel dos Santos <danieldossantxs@gmail.com>
2026-04-24 09:22:00 -0300

    Segurança: adicionar [Authorize] nos controllers sem autenticação
    CargosController, ColaboradoresController, ClientesController,
    EtapasController, SetoresController e PrioridadesController estavam
    acessíveis sem autenticação. Requisições não autenticadas agora recebem 401.

--- Backend/API/Controllers/SetoresController.cs
+++ Backend/API/Controllers/SetoresController.cs
@@ -1,6 +1,7 @@
 ﻿using API.DB;
 using API.DTOs.Setores;
 using API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
@@ -9,6 +10,7 @@ namespace API.Controllers
     /// <summary>
     /// Controlador responsável por gerenciar os setores e departamentos da organização.
     /// </summary>
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     [Produces("application/json")]
